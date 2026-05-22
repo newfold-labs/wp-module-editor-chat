42e318f218ae6f4b6c6b33e20e5ef92902a60c03
Francesco Leanza <leanzafrancesco@gmail.com>
2026-05-22 16:19:43 +0200

    Apply Figma design to the plan upgrade banner

--- src/components/editor-enhancer/plan-upgrade-banner/PlanUpgradeBanner.tsx
+++ src/components/editor-enhancer/plan-upgrade-banner/PlanUpgradeBanner.tsx
@@ -13,31 +13,18 @@ const options = getPlanUpgradeBannerOptions();
 const rootElement = document?.querySelector("#wpwrap") as HTMLDivElement;
 
 export default function PlanUpgradeBanner() {
-	const [dismissed, setDismissed] = useState(false);
-
-	if (!options || dismissed || !rootElement) {
+	if (!options || !rootElement) {
 		return null;
 	}
-
-	const dismiss = () => {
-		document.body.classList.remove("nfd-editor-chat--has-plan-upgrade-banner");
-		setDismissed(true);
-	};
 
 	const banner = (
 		<>
 			<div className="nfd-editor-chat__plan-upgrade-banner">
 				<div className="nfd-editor-chat__plan-upgrade-banner__content">
 					<div className="nfd-editor-chat__plan-upgrade-banner__message">{options.message}</div>
-
-					<div className="nfd-editor-chat__plan-upgrade-banner__actions">
-						<a className="nfd-editor-chat__plan-upgrade-banner__cta" href={options.upgradeUrl}>
-							{__("Upgrade now", "wp-module-editor-chat")}
-						</a>
-						<div className="nfd-editor-chat__plan-upgrade-banner__dismiss" onClick={dismiss}>
-							{__("Dismiss", "wp-module-editor-chat")}
-						</div>
-					</div>
+					<a className="nfd-editor-chat__plan-upgrade-banner__cta" href={options.upgradeUrl}>
+						{__("Upgrade now", "wp-module-editor-chat")}
+					</a>
 				</div>
 			</div>
 		</>
